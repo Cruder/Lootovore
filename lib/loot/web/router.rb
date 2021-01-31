fd58e9fb66b3d2fc788aa7c5c62df594bd226888
Alexandre Lairan <alexandre@inthememory.com>
2021-01-31 19:13:13 +0100

    Add import

--- lib/loot/web/router.rb
+++ lib/loot/web/router.rb
@@ -6,6 +6,8 @@ module Loot
       root to: Controllers::Root::Index
 
       get '/loots', to: Controllers::Loots::Index
+      post '/loots/imports', to: Controllers::Loots::Imports
+
       get '/characters', to: Controllers::Characters::Index
       get '/characters/:id', to: Controllers::Characters::Show
 
